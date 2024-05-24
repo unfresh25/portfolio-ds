f68b3d135e21b585b287839a048441c1f188bc95
Jorge Borja <jborjaa@uninorte.edu.co>
2024-05-24 05:02:30 -0500

    Added experience and skills section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'loop-scroll': 'loop-scroll 30s linear infinite'
+			},
+			keyframes: {
+				'loop-scroll': {
+					'0%': { transform: 'translateX(0)' },
+					'100%': { transform: 'translateX(-100%)' }
+				}
+			}
+		},
 		backgroundSize: {
 			'2x': '97px'
 		}
