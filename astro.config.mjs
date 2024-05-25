5b1775671c8a237095d099abf1c29b6be66721a2
Jorge Borja <jborjaa@uninorte.edu.co>
2024-05-24 21:24:34 -0500

    Added vercel config for astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'static',
+  adapter: vercel({
+    imageService: true,
+  }),
 });
