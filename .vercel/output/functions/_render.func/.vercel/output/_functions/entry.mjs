d703bc82260221518c40481ca5bcbf1ed40ecac0
Jorge Borja <jorgeborja48@gmail.com>
2025-07-18 00:34:01 -0500

    Updated dependencies

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,26 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_CRfCw_8u.mjs';
+import { manifest } from './manifest_z5auuZeE.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/index.astro", _page1]
+]);
+const serverIslandMap = new Map();
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "e08e7256-ad24-4dff-a327-bd9916159710",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
